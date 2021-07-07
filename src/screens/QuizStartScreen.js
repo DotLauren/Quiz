9aa4a99c1e42f712831a6277490516f92a2e03d5
S-Lauren <sarah.ghanem.simplon@gmail.com>
2021-07-07 16:38:04 +0200

    quizz almost done :sparkles:

--- src/screens/QuizStartScreen.js
+++ src/screens/QuizStartScreen.js
@@ -1,10 +1,92 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const QuizStartScreen = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+  ];
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const question = questions.map((q) => q.questionText);
   return (
     <View style={styles.container}>
-      <Text>Hello QuizStart !</Text>
+      <Text style={styles.title}>Hello QuizStart !</Text>
+
+      {showScore ? (
+        <Text>
+          You scored {score} out of {questions.length}
+        </Text>
+      ) : (
+        <>
+          <Text style={styles.title}>
+            {questions[currentQuestion].questionText}
+          </Text>
+          <View>
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() =>
+                    handleAnswerOptionClick(answerOption.isCorrect)
+                  }
+                >
+                  <Text>{answerOption.answerText}</Text>
+                </TouchableOpacity>
+              )
+            )}
+          </View>
+        </>
+      )}
     </View>
   );
 };
@@ -16,5 +98,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  question: {
+    fontSize: 24,
   },
 });
